91b36c7f10c51fa78bb3ffe943eb990d59a1d976
sara toftegaard <satoftegaard@gmail.com>
2016-12-13 14:30:24 -0500

    finished

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import Slider from './Slider.js'
+// import Slider from './Slider.js'
 
 class App extends Component {
 
@@ -38,19 +38,19 @@ class App extends Component {
   }
 
   render () {
-    const hsla = `hsla(${this.state.hue}, ${this.state.saturation}%, ${this.state.lightness}%, ${this.state.alpha / 100})`
+    const hsla = `hsla(${this.state.hue}, ${this.state.saturation}%, ${this.state.lightness}%, ${this.state.alpha})`
     return <div>
       <h1>HSL Color Selector</h1>
       <figure>
         <div style={{ backgroundColor: hsla }} />
       </figure>
-      <p>hsla 0,0,0,0</p>
+      <p>{hsla}</p>
       <form>
         <ul>
-          <li><p>Hue</p><input name='hue' type='range' min='0' max='360' /> </li>
-          <li><p>Saturation</p><input name='sat' type='range' min='0' max='100' /> </li>
-          <li><p>Lightness</p><input name='light' type='range' min='0' max='100' /> </li>
-          <li><p>Alpha</p><input name='alpha' type='range' min='0.00' max='1.00' step='.01' /> </li>
+          <li><p>Hue</p><input value={this.state.hue} name='hue' type='range' min='0' max='360' onInput={(event) => { this.updateHue(event.target.value) }} /> </li>
+          <li><p>Saturation</p><input value={this.state.saturation} name='sat' type='range' min='0' max='100' onInput={(event) => { this.updateSaturation(event.target.value) }} /> </li>
+          <li><p>Lightness</p><input value={this.state.lightness} name='light' type='range' min='0' max='100' onInput={(event) => { this.updateLightness(event.target.value) }} /> </li>
+          <li><p>Alpha</p><input value={this.state.alpha} name='alpha' type='range' min='0.00' max='1.00' step='.01' onInput={(event) => { this.updateAlpha(event.target.value) }} /> </li>
         </ul>
       </form>
     </div>
